324920310536d24700f0b0da7b4588a6847287ce
Hassan Khalil <146346219+HassanKhalil321@users.noreply.github.com>
2023-11-27 19:19:11 +0200

    Update learn.dart

--- lib/learn.dart
+++ lib/learn.dart
@@ -1,14 +1,1 @@
-import 'package:flutter/material.dart';
-class Learn extends StatefulWidget {
-  const Learn({super.key});
 
-  @override
-  State<Learn> createState() => _LearnState();
-}
-
-class _LearnState extends State<Learn> {
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
